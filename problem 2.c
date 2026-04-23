d07f4cf926fd2cef20f098a1a3d20fa1f7c38b60
harshakichu07-prog <harshakichu.07@gmail.com>
2026-04-23 19:33:20 +0530

    problem 2.c

---
+++ problem 2.c
@@ -0,0 +1,46 @@
+10. Regular Expression Matching
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+Return a boolean indicating whether the matching covers the entire input string (not partial).
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isMatch(char* s, char* p) {
+    int m = strlen(s), n = strlen(p);
+    bool dp[m + 1][n + 1];
+
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) dp[i][j] = false;
+    }
+
+    dp[0][0] = true;
+
+    for (int j = 2; j <= n; j++) {
+        if (p[j - 1] == '*') dp[0][j] = dp[0][j - 2];
+    }
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else if (p[j - 1] == '*') {
+                dp[i][j] = dp[i][j - 2];
+                if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j];
+                }
+            }
+        }
+    }
+    return dp[m][n];
+}
+
+int main() {
+    char s[] = "aa", p[] = "a*";
+    if (isMatch(s, p)) printf("true\n");
+    else printf("false\n");
+    return 0;
+}
